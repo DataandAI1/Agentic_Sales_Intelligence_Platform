06216cd105aa06c601e173237cc6454b395d6477
DataandAI1 <dataandaiinnovations@gmail.com>
2025-08-28 10:25:28 -0700

    Add design system colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+// Design System Colors
+export const colors = {
+  primary: '#00D4AA',
+  primaryDark: '#00A886',
+  navy: '#0A1628',
+  navyLight: '#1C2B3D',
+  white: '#FFFFFF',
+  grayLight: '#F5F5F5',
+  grayMedium: '#949494',
+  grayDark: '#333333',
+  success: '#4CAF50',
+  warning: '#FF9800',
+  error: '#F44336',
+  info: '#2196F3',
+  purple: '#5D3C81',
+  purpleLight: '#8B5FC7'
+};
